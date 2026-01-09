ac53a754b3f1f025a49090510bf7518da9ae9636
Max Snischuk <m.snischuk@gmail.com>
2026-01-09 15:26:24 +0200

    feat: add queuePromises

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -135,8 +135,12 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuePromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuePromises(promises) {
+  return promises.reduce((prevPromise, currPromise) => {
+    return prevPromise.then((prevResult) => {
+      return currPromise.then((currValue) => prevResult + currValue);
+    });
+  }, Promise.resolve(''));
 }
 
 module.exports = {
